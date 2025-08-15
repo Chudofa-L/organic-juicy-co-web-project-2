8115ec17e190066d7e2ed8e1df12717e0a7d8568
ljuba@Lyba00 <ljuba650@gmail.com>
2025-08-15 06:30:23 +0300

    added sliders

--- js/script.js
+++ js/script.js
@@ -9,3 +9,62 @@ menuIcon.addEventListener('click', () => {
   body.classList.toggle('no-scroll');
 });
 
+//------------- Слайдер стрелки-----------------//
+
+const sliderArrows = document.querySelector('.slider-arrows'),
+      slidesArrows = sliderArrows.querySelectorAll('.slider-arrows__items'),
+      prev = sliderArrows.querySelector('.slider-arrows__arrow--left'),
+      next = sliderArrows.querySelector('.slider-arrows__arrow--right');
+
+let slideIndex = 0;
+
+prev.addEventListener('click', () => showSliderArrows(-1));
+
+next.addEventListener('click', () => showSliderArrows(1));
+
+function showSliderArrows(n = 0) {
+  slideIndex += n;
+
+  if (slideIndex < 0) slideIndex = slidesArrows.length - 1;
+  if (slideIndex >= slidesArrows.length) slideIndex = 0;
+
+
+
+  slidesArrows.forEach(item => item.style.display = 'none');
+  slidesArrows[slideIndex].style.display = 'block';
+}
+
+showSliderArrows();
+
+// -------------------------Слайдер точки -----------------
+
+const sliderDots = document.querySelector('.slider-dots'),
+      slidesDots = sliderDots.querySelectorAll('.slider-dots__items'),
+      wrapperDots = document.querySelector('.slider-dots__nav');
+
+const dots = [];
+
+for (let i = 0; i < slidesDots.length; i++) {
+  const dot =document.createElement('button');
+  dot.dataset.slideTo = i;
+  dot.classList.add('slider-dots__nav-item');
+  if (i == 0) dot.classList.add('slider-dots__nav-item--active');
+
+  if (i != 0) slidesDots[i].style.display = 'none';
+
+  dot.addEventListener('click', showSlideDots)
+
+  wrapperDots.append(dot);
+  dots.push(dot);
+}
+
+function showSlideDots (event) {
+  const slideTo = event.target.dataset.slideTo;
+
+  slidesDots.forEach(item => item.style.display = 'none');
+  slidesDots[slideTo].style.display = 'block';
+
+  dots.forEach(dot => dot.classList.remove('slider-dots__nav-item--active'));
+  event.target.classList.add('slider-dots__nav-item--active');
+}
+
